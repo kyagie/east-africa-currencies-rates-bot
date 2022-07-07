b8ae4421218fd6fbaf8de083c2c50d940c15b0d7
kyagie <kyagieboy99@gmail.com>
2022-07-07 21:07:03 +0300

    Log

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -3,22 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TwitterController extends Controller
 {
     //
     public function index(Request $request)
     {
+        Log::info("twitter-logs", [
+            $request
+        ]);
+
         // Checks if requests contains crc_token from Twitter servers.
         if ($request->has('crc_token')) {
 
             $hash = hash_hmac("sha256", $request->crc_token, env("CONSUMER_SECRET"), true);
-            return response()->json([
+            return response()->json(
+                [
                     "response_token" => "sha256=" . base64_encode($hash)
                 ]
             );
-
         }
     }
-    
 }
